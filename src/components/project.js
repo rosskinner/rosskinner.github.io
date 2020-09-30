9e208cebbcd7d6c772b164e07ad31da3833fe311
Rosalind <ros.skinner@gmail.com>
2020-09-30 11:03:06 +1000

    updated images and gif and copy

--- src/components/project.js
+++ src/components/project.js
@@ -18,9 +18,9 @@ export default function ProjectPage (props) {
     const l = []
     const d = []
     const i = []
-    data.stack.forEach((item, key) => {
+    data.tech.forEach((item, key) => {
       let spacer = ','
-      if (key === data.stack.length - 1) spacer = ''
+      if (key === data.tech.length - 1) spacer = ''
       s.push(<span key={key}>{item}{spacer} </span>)
     })
     setStack(s)
@@ -43,7 +43,7 @@ export default function ProjectPage (props) {
   const onLoad = () => {
     // count++
     setloadedImages(loadedImages + 1)
-    if (loadedImages + 1 === project.images.length + 1) {
+    if (loadedImages + 1 === project.images.flat().length) {
       setloaded(true)
     }
   }
@@ -67,46 +67,89 @@ export default function ProjectPage (props) {
       <Loader loaded={loaded} />
     {projectLoaded &&
       <div className='project-container page w-100 ph0 ph4-l pb4'>
-        <div className='project-title tr'>
+        {/* <div className='project-title tr'>
           <h1 className='pr5 f4 f3-l georgia'>{project.title}</h1>
-        </div>
+        </div> */}
         <div className='w-100 flex items-center justify-center project-hero'>
           <div className='w-100 mwImage mh4 mh0-l'>
             <img className='w-100 mv2' onLoad={onLoad} src={require(`../projects${project.permalink}/${project.hero}`).default} />
           </div>
         </div>
+        <div>
+          <h1 className='pr5 f4 f2-l georgia'>{project.title}</h1>
+        </div>
         <div className='w-100 flex flex-column justify-center items-center'>
-          <div className='project-content flex flex-column flex-row-l w-100-l mh4 mh0-l mt5 mb3 f4 f3-l'>
-          
+          <div className='project-content flex flex-column flex-row-l w-100-l mh4 mh0-l mt5 mb3 f4 f4-l'>
             <div className='w-100 w-40-l mt3 mt0-l pr2-l'>
               <p>
                 <strong>Date:</strong> {project.date}
               </p>
               <p>
+                <strong>Client:</strong> {project.client}
+              </p>
+
+              <p>
+                <strong>Agency:</strong> {project.agency}
+              </p>
+              <p>
                 <strong>Info:</strong> {project.info}
               </p>
+
+              <p>
+                <strong>Role:</strong> {project.role}
+              </p>
+            </div>
+            
+            <div className='w-100 w-60-l pl4-l'>
               <p>
                 <strong>Deliverables:</strong> {delivery}
               </p>
               <p>
-                <strong>Stack:</strong> {stack}
+                <strong>Tech:</strong> {stack}
               </p>
               <p>
                 <strong>Links:</strong> {link}
               </p>
-            </div>
-            
-            <div className='w-100 w-60-l'>
-              <p className='f4 f3-l'>
+
+              <p className='f4 f4-l mt5'>
                 {project.description}
               </p>
+              
             </div>
           </div>
+          {project.video &&
+            <div className='flex flex-column w-100 video-container mt0 mt4-l mb0 mb3-l'>
+
+            <iframe
+            className='video'
+            width="100%"
+            height="100%"
+            src={`https://www.youtube.com/embed/${project.video}?rel=0&amp;showinfo=0;modestbranding=1`}
+            frameBorder="0"
+            allow="autoplay; encrypted-media"
+            allowFullScreen
+          ></iframe>
+            <video  onLoad={onLoad} src={project.video} />
+          </div>
+          }
+          
           <div className='flex flex-column w-100'>
             {project.images.map((image, key) => {
                let padding = 'mb0 mb3-l'
-               if (key === project.images.length -1) padding = ''
-               return <img className={`w-100 mt0 mt4-l ${padding}`} onLoad={onLoad} key={key} src={require(`../projects${project.permalink}/${image}.jpg`).default} />
+               if (Array.isArray(image)) {
+                 const group = []
+                  image.map((im, keyn) => {
+                    let pad = ''
+                    if (keyn % 2 == 0) pad = 'mr0 mr4-l'
+                    group.push(<img className={`portrait mt0 mt4-l ${padding} ${pad} portrait`} onLoad={onLoad} key={keyn} src={require(`../projects${project.permalink}/${im}`).default} />) 
+                  })
+                  return <div key={key} className='flex flex-row w-100 flex-wrap justify-between'>{group}</div>
+                
+               } else {
+                if (key === project.images.length -1) padding = ''
+                return <img className={`w-100 mt0 mt4-l ${padding}`} onLoad={onLoad} key={key} src={require(`../projects${project.permalink}/${image}.jpg`).default} />
+               }
+               
             })}
           </div>
         </div>
